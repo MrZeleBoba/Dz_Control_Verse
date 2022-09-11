d07371537fcc579fe87aacfcebe6d03dfd9a44c6
Aleksey Galishchev <galishchev@inbox.ru>
2022-09-11 15:21:22 +0300

    getAverage add

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,14 @@
 public class Main {
 
     public static void main(String[] args) {
-        int[] arr = {100, 200, 300};
+        int[] arr = {100, 200, 300, 500, 400};
         SalesManager salesManager = new SalesManager(arr);
 
         int max = salesManager.max();
 
         System.out.println(max);
+
+        System.out.println(salesManager.getAverage());
 
     }
 }
